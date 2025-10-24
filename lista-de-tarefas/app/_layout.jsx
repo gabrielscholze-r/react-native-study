9b69d6f5847b3dbdc4143fd7ca736fd7107388d1
gabrielscholze-r <gabrielscholze24@gmail.com>
2025-10-23 21:06:26 -0300

    tasklist-6

---
+++ lista-de-tarefas/app/_layout.jsx
@@ -0,0 +1,75 @@
+import { Alert, Image, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import logo from "../assets/images/check.png";
+import { colors } from "../constants/colors";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaView style={style.mainContainer}>
+
+      <View style={style.rowContainer}>
+        <Image source={logo} style={style.image} />
+        <Text style={style.title}>Minhas Tarefas</Text>
+      </View>
+
+      <View style={style.rowContainer}>
+        <TextInput style={style.input} />
+        <Pressable
+          onPress={() => Alert.alert("Oi")}
+          style={({ pressed }) => [
+            style.button,
+            { backgroundColor: pressed ? "blue" : colors.primary }
+          ]}
+        >
+          <Text style={style.buttonText}>+</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+
+const style = StyleSheet.create({
+  image: {
+    width: 50,
+    height: 50
+  },
+  title: {
+    fontSize: 30,
+    fontFamily: "Calibri",
+    fontWeight: 600,
+    color: colors.primary
+  },
+  input: {
+    height: 40,
+    paddingHorizontal: 16,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 20,
+    flexGrow: 1
+  },
+  button: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: colors.primary,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20
+  },
+  mainContainer: {
+    margin: 20
+  },
+  rowContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    marginBottom: 20
+  }
+})
